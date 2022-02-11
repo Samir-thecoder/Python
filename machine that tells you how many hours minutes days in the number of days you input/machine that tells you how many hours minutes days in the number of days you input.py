959f81e2822c313846441d7b0768c0ccddf1afdb
Samir-thecoder <98772398+Samir-thecoder@users.noreply.github.com>
2022-02-11 18:01:21 +0400

    Add files via upload

---
+++ machine that tells you how many hours minutes days in the number of days you input/machine that tells you how many hours minutes days in the number of days you input.py
@@ -0,0 +1,7 @@
+d=int(input('Please enter the number of days'))
+h=d*24
+m=d*1440
+s=d*86400
+print('The number of hours in ',d,' days are ', h)
+print('The number of minutes in ',d,' days are ', m)
+print('The number of seconds in ',d,' days are ', s)
